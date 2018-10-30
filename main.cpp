87c77dc51b016a7af2197627ca10bd4632d08b9a
Umang Parmar <umangjparmar@gmail.com>
2018-10-30 18:43:24 +0000

    Fix compile errors

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <cuda_runtime.h>
-#include "include/load_save.h"
-#include "include/blur_ops.h"
+#include "load_save.h"
+#include "blur_ops.h"
 
 using namespace std;
 
 size_t numRows, numCols;
 
-uchar4* load_image_in_GPU(string filename) { 
+uchar4* load_image_in_GPU(string filename) {
 	// Load the image into main memory
-	uchar4 *h_image, *d_in;
+	uchar4 *h_image = NULL, *d_in = NULL;
 	loadImageRGBA(filename, &h_image, &numRows, &numCols);
-  // Allocate memory to the GPU
+ 	// Allocate memory to the GPU
 	cudaMalloc((void **) &d_in, numRows * numCols * sizeof(uchar4));
 	cudaMemcpy(d_in, h_image, numRows * numCols * sizeof(uchar4), cudaMemcpyHostToDevice);
 	// No need to keep this image in RAM now.
-	free(h_image);
+	delete h_image;
 	return d_in;
 }
 
-int hex_to_int(string hexa) {
+int hex_to_int(string hexStr) {
 	int i;
-	stringstream s(hexa);
-	s>>std::hex>>i;
+	stringstream ss;
+    	ss << std::hex << hexStr;
+	ss >> i;
 	return i;
 }
 
@@ -35,24 +36,19 @@ uchar4 hex_to_uchar4_color(string& color) {
 
 int main(int argc, char **argv) {
 
-	if(argc < 2){
-		cerr<<"Please specify the input file's name!\n";
-		exit(1);
-	}
-	string input_file = string(argv[1]);
-	string output_file = vm["output"].as<string>();
-
+	string input_file = "gpu.jpg";
+	string output_file = "d_out.jpg";
 	uchar4 *d_in = load_image_in_GPU(input_file);
 	uchar4 *h_out = NULL;
 
 	// Performing the required operation
-	int amount = vm["amount"].as<int>();
+	int amount = 20;
 	if(amount % 2 == 0)
 		amount++;
-	h_out = blur_ops(d_in, numRows, numCols, amount);                      
+	h_out = blur_ops(d_in, numRows, numCols, amount);
 
 	cudaFree(d_in);
 	if(h_out != NULL)
-		saveImageRGBA(h_out, output_file, numRows, numCols); 
+		saveImageRGBA(h_out, output_file, numRows, numCols);
 
 }
